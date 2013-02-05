6986196fa3d2309ef35159bc0a24fdfe7709ab45
vagrant <vagrant@precise64.(none)>
2013-02-05 14:03:00 +0000

    Fix broken robots.txt

--- bandb/urls.py
+++ bandb/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.http import HttpResponse
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
