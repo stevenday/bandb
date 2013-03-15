047f938198f521bdc3bea55f0887b1c49b89f466
Steven Day <forestorama@gmail.com>
2013-03-15 12:44:47 +0000

    Add inuit.css and move static files to their own directory

--- bandb/settings/base.py
+++ bandb/settings/base.py
@@ -57,7 +57,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PARENT_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -68,7 +68,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, 'bandb', 'web'),
+    os.path.join(PROJECT_ROOT, 'web'),
 )
 
 # List of finder classes that know how to find static files in
