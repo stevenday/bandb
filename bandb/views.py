e861152d8f52f72e3a77a6fa4419aadc6dd7c81e
Steven Day <forestorama@gmail.com>
2013-03-18 16:43:40 +0000

    add new views and urls

--- bandb/views.py
+++ bandb/views.py
@@ -3,3 +3,24 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+class HutView(TemplateView):
+    template_name = 'hut.html'
+
+class RatesView(TemplateView):
+    template_name = 'rates.html'
+
+class AreaView(TemplateView):
+    template_name = 'area.html'
+
+class AvailabilityView(TemplateView):
+    template_name = 'availability.html'
+
+class ThingsToKnowView(TemplateView):
+    template_name = 'things-to-know.html'
+
+class FindView(TemplateView):
+    template_name = 'find.html'
+
+class GalleryView(TemplateView):
+    template_name = 'gallery.html'
