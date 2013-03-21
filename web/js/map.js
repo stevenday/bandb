99e8e8c41fb1d65d67a702ee686a6c9ea9265d90
Steven Day <forestorama@gmail.com>
2013-03-21 22:14:59 +0000

    Add directions and improve the map centering

--- web/js/map.js
+++ web/js/map.js
@@ -1,14 +1,15 @@
 (function(window, L){
-    var latLon = [50.985397, -2.174799]
+    var centerLatLon = [50.994, -2.1746]
+    var locationLatLon = [50.985397, -2.174799]
 
-    var map = L.map('map').setView(latLon, 14);
+    var map = L.map('map').setView(centerLatLon, 13);
 
     L.tileLayer('http://{s}.tile.cloudmade.com/' + window.TilleysHut.CLOUDMADE_API_KEY + '/997/256/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
         maxZoom: 18
     }).addTo(map);
 
-    L.marker(latLon).addTo(map)
+    L.marker(locationLatLon).addTo(map)
         .bindPopup('Tilley\'s Hut')
         .openPopup();
  })(window, window.L);
