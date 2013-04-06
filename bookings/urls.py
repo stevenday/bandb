470b019b8c0dd2721b01350878cf52c2a590fbe7
Steven Day <forestorama@gmail.com>
2013-04-06 16:14:08 +0000

    Fix the lack of year/month kwargs on the booking form, and move the calendars above the personal info

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,6 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 from .views import *
 
 urlpatterns = patterns('',
-    url(r'^new$', CreateBooking.as_view(), name='booking'),
+    url(r'^new(?:/(?P<year>\d{4})/(?P<month>\d{2}))?$', CreateBooking.as_view(), name='booking'),
     url(r'^(?P<pk>\d+)/pay$', csrf_exempt(PayForBooking.as_view()), name='payment')
 )
